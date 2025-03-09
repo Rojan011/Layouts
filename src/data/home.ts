fac9b5d7be946f69d8fe3aebee316c4a62e6db97
Rojan <shrestharojan79@gmail.com>
2025-03-09 19:02:27 +0545

    Video Section Complete

--- src/data/home.ts
+++ src/data/home.ts
@@ -2,13 +2,13 @@ export const categories = [
   "All",
   "JavaScript",
   "TypeScript",
-  "Programming",
+  "The Big Bang Theory",
   "Weight Lifting",
   "Bowling",
-  "Hiking",
+  "LeBron James",
   "React",
   "Next.js",
-  "Functional Programming",
+  "Chair Aur Code",
   "Object Oriented Programming",
   "Frontend Web Development",
   "Backend Web Development",
@@ -19,9 +19,9 @@ export const categories = [
 export const videos = [
   {
     id: "1",
-    title: "CSS Anchor Is The Best New CSS Feature Since Flexbox",
+    title: "LeBron James's Kingly Triple Double",
     channel: {
-      name: "Web Dev Simplified",
+      name: "King James",
       id: "WebDevSimplified",
       profileUrl:
         "https://yt3.ggpht.com/ytc/APkrFKZWeMCsx4Q9e_Hm6nhOOUQ3fv96QGUXiMr1-pPP=s48-c-k-c0x00ffffff-no-rj",
@@ -29,15 +29,16 @@ export const videos = [
     views: 222536,
     postedAt: new Date("2023-08-29"),
     duration: 938,
-    thumbnailUrl: "https://i.ytimg.com/vi/B4Y9Ed4lLAI/maxresdefault.jpg",
+    thumbnailUrl:
+      "https://images2.minutemediacdn.com/image/upload/c_crop,w_4740,h_2666,x_0,y_324/c_fill,w_720,ar_16:9,f_auto,q_auto,g_auto/images/ImagnImages/mmsport/fastbreak/01jbfwtyb859tx8xnzha.jpg",
     videoUrl:
       "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
   },
   {
     id: "2",
-    title: "NEW Way To Create Variables In JavaScript",
+    title: "Math Science History Unraveling The Mystery!",
     channel: {
-      name: "Web Dev Simplified",
+      name: "The Big Bang Theory",
       id: "WebDevSimplified",
       profileUrl:
         "https://yt3.ggpht.com/ytc/APkrFKZWeMCsx4Q9e_Hm6nhOOUQ3fv96QGUXiMr1-pPP=s48-c-k-c0x00ffffff-no-rj",
@@ -45,23 +46,25 @@ export const videos = [
     views: 257136,
     postedAt: new Date("2023-08-22"),
     duration: 732,
-    thumbnailUrl: "https://i.ytimg.com/vi/d6a8RymS1zI/maxresdefault.jpg",
+    thumbnailUrl:
+      "https://beam-images.warnermediacdn.com/BEAM_LWM_DELIVERABLES/c8ea8e19-cae7-4683-9b62-cdbbed744784/914da85b-244a-11ef-8e04-12093494333d?host=wbd-images.prod-vod.h264.io&partner=beamcom",
     videoUrl:
       "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
   },
   {
     id: "3",
-    title: "Front-end developer takes on a CSS battle",
+    title: "Bhuvi Does it Again!!!",
     channel: {
       name: "Kevin Powell",
       id: "KevinPowell",
       profileUrl:
-        "https://yt3.ggpht.com/ytc/APkrFKa6XiLa13mMVPzkmmTBcgNPjjqCGPrY86KfJFmf5w=s48-c-k-c0x00ffffff-no-rj",
+      "https://yt3.ggpht.com/ytc/APkrFKZWeMCsx4Q9e_Hm6nhOOUQ3fv96QGUXiMr1-pPP=s48-c-k-c0x00ffffff-no-rj",
+       
     },
     views: 1232300,
     postedAt: new Date("2023-10-05"),
     duration: 120,
-    thumbnailUrl: "https://i.ytimg.com/vi/eYPyIq5Y3Rk/maxresdefault.jpg",
+    thumbnailUrl: "https://sport360.com/wp-content/uploads/2019/01/Bhuvi.jpg",
     videoUrl:
       "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
   },
@@ -72,14 +75,16 @@ export const videos = [
       name: "Kevin Powell",
       id: "KevinPowell",
       profileUrl:
-        "https://yt3.ggpht.com/ytc/APkrFKa6XiLa13mMVPzkmmTBcgNPjjqCGPrY86KfJFmf5w=s48-c-k-c0x00ffffff-no-rj",
+        "https://yt3.ggpht.com/ytc/APkrFKZWeMCsx4Q9e_Hm6nhOOUQ3fv96QGUXiMr1-pPP=s48-c-k-c0x00ffffff-no-rj",
+        
     },
     views: 112,
     postedAt: new Date("2023-9-19"),
     duration: 4343,
     thumbnailUrl: "https://i.ytimg.com/vi/apdD69J4bEc/maxresdefault.jpg",
     videoUrl:
-      "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
+    "https://yt3.ggpht.com/ytc/APkrFKZWeMCsx4Q9e_Hm6nhOOUQ3fv96QGUXiMr1-pPP=s48-c-k-c0x00ffffff-no-rj",
+      
   },
   {
     id: "5",
@@ -99,49 +104,54 @@ export const videos = [
   },
   {
     id: "6",
-    title: "I Got Laid Off...",
+    title: "The Rookie",
     channel: {
       name: "Caleb Curry",
       id: "CalebCurry",
       profileUrl:
-        "https://yt3.googleusercontent.com/ytc/APkrFKbpSojje_-tkBQecNtFuPdSCrg3ZT0FhaYjln9k0g=s176-c-k-c0x00ffffff-no-rj",
+      "https://yt3.ggpht.com/ytc/APkrFKZWeMCsx4Q9e_Hm6nhOOUQ3fv96QGUXiMr1-pPP=s48-c-k-c0x00ffffff-no-rj",
+       
     },
     views: 10340,
     postedAt: new Date("2023-03-03"),
     duration: 54,
-    thumbnailUrl: "https://i.ytimg.com/vi/i2JVQdLnkAY/maxresdefault.jpg",
+    thumbnailUrl:
+      "https://cdn1.edgedatg.com/aws/v2/abc/TheRookie/showimages/7774f0b9ee912a9b31216b58e6142757/2688x1076-Q75_7774f0b9ee912a9b31216b58e6142757.jpg",
     videoUrl:
       "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
   },
   {
     id: "7",
-    title: "Tails OS in 100 Seconds",
+    title: "Chai Aur Node",
     channel: {
-      name: "Fireship",
-      id: "Fireship",
+      name: "Hitesh Choudhary",
+      id: "Hitesh Choudhary",
       profileUrl:
-        "https://yt3.googleusercontent.com/ytc/APkrFKb--NH6RwAGHYsD3KfxX-SAgWgIHrjR5E4Jb5SDSQ=s176-c-k-c0x00ffffff-no-rj",
+        "https://yt3.ggpht.com/ytc/APkrFKZWeMCsx4Q9e_Hm6nhOOUQ3fv96QGUXiMr1-pPP=s48-c-k-c0x00ffffff-no-rj",
+        
     },
     views: 10323340,
     postedAt: new Date("2023-08-09"),
     duration: 100,
-    thumbnailUrl: "https://i.ytimg.com/vi/mVKAyw0xqxw/maxresdefault.jpg",
+    thumbnailUrl:
+      "https://imgproxy.learnyst.com/learnyst-user-assets/school-assets/schools/171024/courses/193290/1715907606917backend_lyst1715907606929.png",
     videoUrl:
       "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
   },
   {
     id: "8",
-    title: "Zig in 100 Seconds",
+    title: "Exercise Tier List",
     channel: {
-      name: "Fireship",
-      id: "Fireship",
+      name: "Jeff Nippard",
+      id: "Jeff Nippard",
       profileUrl:
-        "https://yt3.googleusercontent.com/ytc/APkrFKb--NH6RwAGHYsD3KfxX-SAgWgIHrjR5E4Jb5SDSQ=s176-c-k-c0x00ffffff-no-rj",
+      "https://yt3.ggpht.com/ytc/APkrFKZWeMCsx4Q9e_Hm6nhOOUQ3fv96QGUXiMr1-pPP=s48-c-k-c0x00ffffff-no-rj",
+       
     },
     views: 20323340,
     postedAt: new Date("2023-09-09"),
     duration: 105,
-    thumbnailUrl: "https://i.ytimg.com/vi/kxT8-C1vmd4/maxresdefault.jpg",
+    thumbnailUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTR5jNbSqCa-oH9_dvSSX-9RLKWOrYrwZoG4jX48MxgTi6hWCyWmDZyssqDgqmCRsq_UvU&usqp=CAU",
     videoUrl:
       "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
   },
