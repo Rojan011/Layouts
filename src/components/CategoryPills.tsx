e890ab023072201f1abbde02286acaa465c761b8
Rojan <shrestharojan79@gmail.com>
2025-03-09 10:46:48 +0545

    Category Pills completed

--- src/components/CategoryPills.tsx
+++ src/components/CategoryPills.tsx
@@ -1,19 +1,49 @@
-import { ChevronLeft } from "lucide-react";
+import { ChevronLeft, ChevronRight } from "lucide-react";
 import { Button } from "./Button";
+import { useEffect, useRef, useState } from "react";
 type CategoriesPillProps = {
   categories: string[];
   selectedCategory: string;
   onSelect: (category: string) => void;
 };
 
+const TRANSLATE_AMOUNT = 200;
+
 export function CategoryPills({
   categories,
   selectedCategory,
   onSelect,
 }: CategoriesPillProps) {
+  const [translate, setTranslate] = useState(300);
+  const [isLeftVisible, setIsLeftVisible] = useState(false);
+  const [isRightVisible, setIsRightVisible] = useState(false);
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (containerRef.current == null) return;
+
+    const observer = new ResizeObserver((entries) => {
+      const container = entries[0]?.target;
+      if (container == null) return;
+
+      setIsLeftVisible(translate > 0);
+      setIsRightVisible(
+        translate + container.clientWidth < container.scrollWidth
+      );
+    });
+
+    observer.observe(containerRef.current);
+
+    return () => {
+      observer.disconnect();
+    };
+  }, [categories, translate]);
   return (
-    <div className="overflow-x-hidden relative">
-      <div className="flex whitespace-nowrap gap-3 transition-transform w-[max-content]">
+    <div ref={containerRef} className="overflow-x-hidden relative">
+      <div
+        className="flex whitespace-nowrap gap-3 transition-transform w-[max-content]"
+        style={{ transform: `translateX(-${translate}px) ` }}
+      >
         {categories.map((category) => (
           <Button
             onClick={() => onSelect(category)}
@@ -26,15 +56,50 @@ export function CategoryPills({
         ))}
       </div>
       {/* For rendering arrow on the left hand side */}
-      <div className="absolute left-0 top-1/2 -translate-y-1/2 bg-gradient-to-r from-white from-50% to-transparent w-24 h-full">
-        <Button
-          variant="ghost"
-          size="icon"
-          className="h-full aspect-square w-auto p-1.5"
-        >
-          <ChevronLeft />
-        </Button>
-      </div>
+      {isLeftVisible && (
+        <div className="absolute left-0 top-1/2 -translate-y-1/2 bg-gradient-to-r from-white from-50% to-transparent w-24 h-full">
+          <Button
+            variant="ghost"
+            size="icon"
+            className="h-full aspect-square w-auto p-1.5"
+            onClick={() => {
+              setTranslate((translate) => {
+                const newTranslate = translate - TRANSLATE_AMOUNT;
+                if (newTranslate <= 0) return 0;
+                return newTranslate;
+              });
+            }}
+          >
+            <ChevronLeft />
+          </Button>
+        </div>
+      )}
+
+      {isRightVisible && (
+        <div className="absolute right-0 top-1/2 -translate-y-1/2 bg-gradient-to-l  from-white from-50% to-transparent w-24 h-full flex justify-end">
+          <Button
+            variant="ghost"
+            size="icon"
+            className="h-full aspect-square w-auto p-1.5"
+            onClick={() => {
+              setTranslate((translate) => {
+                if (containerRef.current == null) {
+                  return translate;
+                }
+                const newTranslate = translate + TRANSLATE_AMOUNT;
+                const edge = containerRef.current.scrollWidth;
+                const width = containerRef.current.clientWidth;
+                if (newTranslate + width >= edge) {
+                  return edge - width;
+                }
+                return newTranslate;
+              });
+            }}
+          >
+            <ChevronRight />
+          </Button>
+        </div>
+      )}
     </div>
   );
 }
